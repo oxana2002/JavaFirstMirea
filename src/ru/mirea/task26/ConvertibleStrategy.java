4341ef824b8f0cec6b8e5738fb6d049564642449
oxana <torosyanoxsana@mail.ru>
2021-12-22 23:13:41 +0300

    task_6

--- src/ru/mirea/task26/ConvertibleStrategy.java
+++
@@ -1,5 +0,0 @@
-package ru.mirea.task26;
-
-public interface ConvertibleStrategy {
-    public void convers(double a);
-}
--- src/ru/mirea/task32/BankOperation02.java
+++
@@ -1,7 +0,0 @@
-package ru.mirea.task32;
-
-public class BankOperation02 extends BankSystemTest {
-    public BankOperation02() {
-        super(new ApplePay());
-    }
-}
