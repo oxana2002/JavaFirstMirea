9afea9a5ab8f3cb68105f40fa83d336350bb4078
oxana <torosyanoxsana@mail.ru>
2021-12-23 00:28:48 +0300

    task_6

--- src/ru/mirea/task11/Guessing.java
+++ src/ru/mirea/task11/Guessing.java
@@ -1,22 +1,23 @@
-package mirea.task11;
+package ru.mirea.task11;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class CardinalDirections extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class Guessing extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Guessing.class.getResource("CardinalDirections.fxml"));
-        primaryStage.setTitle("CardinalDirections");
+        FXMLLoader fxmlLoader = new FXMLLoader(Guessing.class.getResource("Guessing.fxml"));
+        primaryStage.setTitle("Угадай число");
         primaryStage.setScene(new Scene(fxmlLoader.load()));
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
